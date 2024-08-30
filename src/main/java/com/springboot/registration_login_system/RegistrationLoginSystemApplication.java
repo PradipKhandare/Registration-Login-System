988c933ba4d3814555b98d5096372790534d22ee
PradipKhandare <pradip.khandare@neutrinotechlabs.com>
2024-08-30 17:40:35 +0530

    Initial commit

---
+++ src/main/java/com/springboot/registration_login_system/RegistrationLoginSystemApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.registration_login_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationLoginSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationLoginSystemApplication.class, args);
+	}
+
+}
